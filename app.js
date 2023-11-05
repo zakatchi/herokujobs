2e21755ce6132d1c724cbf7ff86dfe9434b1b385
Argel Rebancos <argel@Argels-MacBook-Pro.local>
2023-11-06 06:22:45 +0800

    fifth attempt

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ app.get('/', function(req, res, next) {
 passport.use(new GoogleStrategy({
   clientID: process.env['GOOGLE_CLIENT_ID'],
   clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
-  callbackURL: '/oauth2/redirect/google',
+  callbackURL: 'https://sc2006-teamname-f30436cbefaa.herokuapp.com/oauth2/redirect/google',
   scope: [ 'profile' ]
 }, function verify(issuer, profile, cb) {
   db.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [
